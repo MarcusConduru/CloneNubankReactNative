e21f1f486e46296d9a950ff02fbac062b6aefb51
marcus vinicius <marcus.conduru@gmail.com>
2021-05-31 23:25:32 -0300

    feat: ensure clone of nubank

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+import { Container,TabsContainer, TabsItem, TabText, } from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default function Tabs({ translateY }) {
+  return (
+    <Container style={{
+      transform: [{
+        translateY: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [0, 30],
+          extrapolate: 'clamp',
+        }),
+      }],
+      opacity: translateY.interpolate({
+        inputRange: [0, 380],
+        outputRange: [1,0.3],
+        extrapolate: 'clamp',
+      }),
+    }}>
+      <TabsContainer>
+        <TabsItem>
+           <Icon name="person-add" size={24} color="#fff" />
+           <TabText>Indicar amigos</TabText>
+        </TabsItem>
+
+        <TabsItem>
+           <Icon name="chat-bubble-outline" size={24} color="#fff" />
+           <TabText>Cobrar</TabText>
+        </TabsItem>
+
+        <TabsItem>
+           <Icon name="arrow-downward" size={24} color="#fff" />
+           <TabText>Depositar</TabText>
+        </TabsItem>
+
+        <TabsItem>
+           <Icon name="arrow-upward" size={24} color="#fff" />
+           <TabText>Transferir</TabText>
+        </TabsItem>
+
+        <TabsItem>
+           <Icon name="lock" size={24} color="#fff" />
+           <TabText>Bloquear cartão</TabText>
+        </TabsItem>
+      </TabsContainer>
+    </Container>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Main from '~/pages/Main';
+
+const Routes = createAppContainer(createSwitchNavigator({ Main }));
+
+export default Routes;
